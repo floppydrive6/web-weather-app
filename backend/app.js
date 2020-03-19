88aaebc7582ca61ba8123d9fb9dacef37dea9ad1
grzegorzprzybylo <grzeg.przybylo@ubnt.com>
2020-03-19 14:47:34 +0100

    bump packages

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,8 @@ const weatherFetchRouter = require('./routes/weatherFetch');
 const app = express();
 
 // view engine setup
+app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(cors());
