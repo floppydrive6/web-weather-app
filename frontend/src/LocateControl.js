1f273c42a961775270967d9e3071cc487863e04e
Grzegorz Przybylo <grzeg.przybylo@ubnt.com>
2020-04-19 15:23:59 +0200

    Add locate sync (#4)
    * add locate sync
    
    * handle location change

--- frontend/src/LocateControl.js
+++ frontend/src/LocateControl.js
@@ -3,8 +3,15 @@ import {withLeaflet, MapControl} from 'react-leaflet';
 import 'leaflet.locatecontrol';
 
 class LocateControl extends MapControl {
+  constructor(props) {
+    super(props);
+    this.handleLocateFound = this.handleLocateFound.bind(this);
+    this.state = {selected: false, lat: 0, lng: 0};
+  }
+
   createLeafletElement(props) {
     const {
+      leaflet: {map},
       ...options
     } = props;
 
@@ -19,13 +26,23 @@ class LocateControl extends MapControl {
       locateOptions: {
         enableHighAccuracy: true
       },
-    });
+    }).addTo(map);
     return lc;
   }
 
   componentDidMount() {
     const {map} = this.props.leaflet;
+    map.on('locationfound', this.handleLocateFound);
     map.addControl(this.leafletElement);
+  }
+
+  handleLocateFound(e) {
+    if (e && e.latlng) {
+      this.setState({located: true, lng: e.latlng.lng, lat: e.latlng.lat});
+      this.props.informParent(this.state);
+    } else {
+      console.error('Error when parsing location data');
+    }
   }
 }
 
