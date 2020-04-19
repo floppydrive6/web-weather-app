5d5070625f29cf4168e9d51fc33aa7addbb04e2c
grzegorzprzybylo <grzeg.przybylo@ubnt.com>
2020-04-19 15:51:02 +0200

    reformat

--- e2e/helpers/customhelper_helper.js
+++ e2e/helpers/customhelper_helper.js
@@ -2,8 +2,6 @@ const {Helper} = codeceptjs;
 const recorder = require('codeceptjs').recorder;
 
 class CustomHelper extends Helper {
-
-
   async flushRetries() {
     await new Promise(r => setTimeout(r, 100));
     while (recorder.retries.length) recorder.retries.pop();
@@ -15,7 +13,6 @@ class CustomHelper extends Helper {
     await this.helpers['Puppeteer'].waitForElement('.results.active', 10);
     await this.helpers['Puppeteer'].click(locate('div').inside(locate('.results.active')).first());
   }
-
 }
 
 module.exports = CustomHelper;
